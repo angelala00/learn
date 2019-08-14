03d55190ca875b6817cdf49c58005bfdd97f588f
jiangchi <jiangchi@58ganji.com>
2019-08-14 09:13:29 +0800

    problem 286 and 705

---
+++ src/main/java/cn/jc/exercise/leetcode/problem705/MyHashSet.java
@@ -0,0 +1,37 @@
+package cn.jc.exercise.leetcode.problem705;
+
+import java.util.Stack;
+class MyHashSet {
+
+    byte[] data = null;
+    /** Initialize your data structure here. */
+    public MyHashSet() {
+        data = new byte[1000000];
+    }
+
+    public void add(int key) {
+        if (!contains(key)) {
+            data[key] = 1;
+        }
+    }
+
+    public void remove(int key) {
+        data[key] = 0;
+    }
+
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        return data[key] == 1;
+    }
+    private int getHash(int key){
+        return key;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
