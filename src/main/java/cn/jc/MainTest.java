edbd22ddd8e87f3ad6d5248ce34865c5025b3afc
jiangchi <jiangchi@58ganji.com>
2019-08-22 17:09:42 +0800

    problems commit

--- src/main/java/cn/jc/MainTest.java
+++ src/main/java/cn/jc/MainTest.java
@@ -1,23 +1,31 @@
 package cn.jc;
 
+import java.util.BitSet;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 public class MainTest {
     public static void main(String[] args){
-        Set<String> seta = new HashSet<>();
+        int a = 1;
+        for (int i = 1; i <= 20 ; i++) {
+            a = a * 4;
+            System.out.println("a=" + a + " i=" + i);
+        }
+        BitSet bs;
+
+//        Set<String> seta = new HashSet<>();
 //        System.out.println(seta.hashCode());
 //        System.out.println(seta.toString());
-        System.out.println(seta);
-        seta.add("sdf");
+//        System.out.println(seta);
+//        seta.add("sdf");
 //        System.out.println(seta.hashCode());
 //        System.out.println(seta.toString());
-        System.out.println(seta);
-        seta.add("sdfa");
+//        System.out.println(seta);
+//        seta.add("sdfa");
 //        System.out.println(seta.hashCode());
 //        System.out.println(seta.toString());
-        System.out.println(seta);
+//        System.out.println(seta);
 
 //        Double.parseDouble("");
 //        ThreadLocal<String> tl1 = new ThreadLocal();
